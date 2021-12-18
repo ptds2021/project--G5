252c0c10edab24ddfbc2605d13072b99f16d98f5
Vhain38 <massimo@finini.ch>
2021-12-18 01:20:59 +0100

    Update test_anime_functions.R
    test update, still does not work at 100% not sure why

--- tests/testthat/test_anime_functions.R
+++ tests/testthat/test_anime_functions.R
@@ -1,10 +1,25 @@
 library(testthat)
+
+#newcommer_recom
 test_that("newcommer_recom input",{
   #checking that age is not character and that it does not work when it is
-  expect_error(newcommer_recom(anime, age = "text", gender = "action", freetime = 60))
+  expect_error(newcommer_recom(anime,
+                               age = "text",
+                               gender = "action",
+                               freetime = 60))
   #checking that free time is not negative and that it does not work when it is
   expect_error(newcommer_recom(freetime = -1))
   #checking that free time is not character and that it does not work when it is
   expect_error(newcommer_recom(freetime = "text"))
 })
+
+test_that("newcommer_recom output",{
+  #checking that age is not character and that it does not work when it is
+  expect_type(newcommer_recom(anime,
+                              age = "text",
+                              gender = "action",
+                              freetime = 60),
+              list)
+})
+
 
