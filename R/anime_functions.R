7dcf61bb0a65a47bb13e3fc717f6ab65a41adb45
Vhain38 <massimo@finini.ch>
2021-12-18 17:33:42 +0100

    Examples addition
    Addition of examples in the functions specific to our application

--- R/anime_functions.R
+++ R/anime_functions.R
@@ -11,6 +11,14 @@
 #'
 #'@return Return a filtered table with the anime depending on the inputs
 #'
+#'@examples
+#'
+#'#loading the data from the package
+#'anime <- tibble(ProjectG5::anime)
+#'
+#'#filtering the table to make propositions to the user through the function
+#'newcommer_recom(anime, 15, "Sport", 30)
+#'
 #'@export
 
 newcommer_recom <- function(data, age, gender, freetime){
@@ -66,6 +74,17 @@ newcommer_recom <- function(data, age, gender, freetime){
 #'
 #'@return Return the cosine similarity value
 #'
+#'@examples
+#'
+#'#setting seed to always have the same results
+#'set.seed(10)
+#'
+#'#creating a matrix with random numbers in between 1 to 10 from a uniform distribution
+#'x <- matrix(runif(10)*10, ncol=5, nrow=4, byrow=FALSE)
+#'
+#'#computing the cosine similarity between
+#'cos_similarity(x[1,], x[3,])
+#'
 #'@export
 
 cos_similarity = function(A,B){
@@ -86,7 +105,7 @@ cos_similarity = function(A,B){
 #'
 #'@param userid user id, set to 999999999 so that there is no conflict with other Id's
 #'@param user_item_matrix matrix created by the function `user_item_matrix`
-#'@param ratings_matrix table of ratings similar to the one we kept
+#'@param ratings_data table of ratings similar to the one we kept
 #'@param n_recommendation number of recommendations wanted
 #'@param threshold threshold level of number of user that gave a score to the anime
 #'@param nearest_neighbors number of neighbors taken into account for the computation
@@ -95,13 +114,29 @@ cos_similarity = function(A,B){
 #'
 #'@import utils
 #'
+#'@examples
+#'
+#'#Simple example to showcase what is happening in the application
+#'
+#'#import dataset from the package
+#'anime_with_ratings <-  tibble::tibble(ProjectG5::anime_with_ratings)
+#'
+#'#selection of the user example
+#'names_selected <- c("Death Note", "Naruto")
+#'y <- tibble::tibble(Name = names_selected, rating = c(10,7))
+#'anime_selected <- dplyr::left_join(anime, temp_tibble, by = c("Name" = "Name")) %>%  filter(Name %in% names_selected) %>%  mutate(user_id = 999999999)
+#'
+#'#creation of the matrix with what the user selected
+#'item_matrix <- user_item_matrix(anime_with_ratings, adding_row = TRUE, row_data = anime_selected)
+#'
+#'user_based_recom(999999999, item_matrix, anime_with_ratings, 5, 1, 10)
 #'
 #'@export
 
 
 user_based_recom = function(userid = 999999999 ,
                             user_item_matrix ,
-                            ratings_matrix ,
+                            ratings_data ,
                             n_recommendation = 5,
                             threshold = 1,
                             nearest_neighbors = 10) {
@@ -130,9 +165,9 @@ user_based_recom = function(userid = 999999999 ,
     top_n(nearest_neighbors, similarity)
 
 
-  watched_anime_user = ratings_matrix$item_id[ratings_matrix$user_id == userid]
+  watched_anime_user = ratings_data$item_id[ratings_data$user_id == userid]
 
-  recommendations = ratings_matrix %>%
+  recommendations = ratings_data %>%
     filter(user_id %in% similar_users$user_id &
              !(item_id %in% watched_anime_user)) %>%
     group_by(item_id, Name, Episodes, Duration) %>%
@@ -153,13 +188,37 @@ user_based_recom = function(userid = 999999999 ,
 #'@author Marie Bellier, Massimo Finini, Meri Likoska, Vania Rodrigues Telo Ramos, Xavier Renger
 #'
 #'@param data data having the user ratings per anime
-#'@param anime_with_ratings the data by default
 #'@param adding_row Variable that is either TRUE if you want the matrix for the user-based recommendation or FALSE if you want the matrix for the item-based recommendation
 #'@param row_data table created by the function `user_data()`
 #'
 #'@return Return a matrix where the x axis include the item_id and the y axis include the user_id. Each line is the score per item per user
 #'
 #'@import tidyr
+#'
+#'@examples
+#'
+#'#import datasets from the package
+#'anime_with_ratings <-  tibble::tibble(ProjectG5::anime_with_ratings)
+#'anime <- tibble::tibble(ProjectG5::anime)
+#'
+#'#creation of the matrix without adding rows
+#'x <- user_item_matrix(anime_with_ratings, adding_row = FALSE)
+#'
+#'#not printed there output would be too long
+#'
+#'
+#'
+#'
+#'
+#'#creation of a table to add rows, userid high on a voluntary basis
+#'y <- tibble::tibble(Name = c("Death Note, Hunter x Hunter"), rating = c(10,7))
+#'
+#'anime_selected <- dplyr::left_join(anime, temp_tibble, by = c("Name" = "Name")) %>%  filter(Name %in% anime_names()) %>%  mutate(user_id = 999999999)
+#'
+#'#creation of the matrix while adding rows
+#'z <- user_item_matrix(anime_with_ratings, adding_row = TRUE, row_data = anime_selected)
+#'
+#'#not printed here, output would be too long
 #'
 #'@export
 
@@ -215,6 +274,19 @@ user_item_matrix <- function(data = anime_with_ratings, adding_row = FALSE, row_
 #'
 #'@return Return a table composed of `n_recommendation` that the user have not seen yet
 #'
+#'@examples
+#'#Simple example to showcase what is happening in the application
+#'
+#'#User selection
+#'selected_item_name = "Naruto"
+#'
+#'#first create the item matrix
+#'anime <-  tibble::tibble(ProjectG5::anime)
+#'anime_with_ratings <-  tibble::tibble(ProjectG5::anime_with_ratings)
+#'item_matrix <- user_item_matrix(anime_with_ratings, adding_row = FALSE)
+#'
+#'#then use it to run the function
+#'item_recommendation(selected_item_name, item_matrix, 5, anime)
 #'
 #'@export
 
