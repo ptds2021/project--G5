d3ea9fea3a5cdf0e3dd66a936073f9abaae79d6d
Vhain38 <massimo@finini.ch>
2021-12-18 19:10:26 +0100

    example correction
    had typo and some minor errors that were creating conflicts in the example, normally solved now

--- R/anime_functions.R
+++ R/anime_functions.R
@@ -216,7 +216,12 @@ user_based_recom = function(userid = 999999999 ,
 #'
 #'
 #'#creation of a table to add rows, userid high on a voluntary basis
-#'temp_tibble <- tibble::tibble(Name = c("Death Note", "Naruto"), rating = c(10,7))
+#'
+#'#selection of the user example
+#'names_selected <- c("Death Note", "Naruto")
+#'score_entered <- c(10,7)
+#'
+#'temp_tibble <- tibble::tibble(Name = names_selected, rating = score_entered)
 #'
 #'anime_selected <- dplyr::left_join(anime, temp_tibble, by = c("Name" = "Name"))
 #'anime_selected <- dplyr::filter(anime_selected, Name %in% names_selected)
